a82e19b15bfe9eff0a014c4e99cbbf8c06cdca70
Konstantinos Volakakis <volakakis@ics.forth.gr>
2023-11-27 17:12:25 +0200

    Minor changes for deployment in aws

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  host: 'http://192.168.1.17:3000'
+  // host: 'http://127.0.0.1:3000'
+  host: 'http://ec2-34-207-196-247.compute-1.amazonaws.com:3000'
 };
