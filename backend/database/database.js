81ef4656b675827ec5c128520857e14618480838
Konstantinos Volakakis <volakakis@ics.forth.gr>
2023-11-27 17:11:49 +0200

    Changed to production mode for deployment

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const path = require("path");
-const config = require(path.resolve("config", "dev.js")); // change dev.js to prod.js for production (dockerized version)
+const config = require(path.resolve("config", "prod.js")); // change dev.js to prod.js for production (dockerized version)
 async function startup() {
+  console.log(config.mongo);
   await mongoose.connect(config.mongo.uri, config.mongo.options);
   console.log("[MongoDB] Database connection succeeded..");
 }
