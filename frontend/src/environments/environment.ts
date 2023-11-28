737c8da64cddd65c476f89a1a4cc09bbefb9f25f
Konstantinos Volakakis <volakakis@ics.forth.gr>
2023-11-28 16:17:13 +0200

    Modified so that frontend can run separately to backend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // host: 'http://127.0.0.1:3000'
-  host: 'http://ec2-34-207-196-247.compute-1.amazonaws.com:3000'
+  host: 'http://127.0.0.1:3000' // add your IP here is you want to test on different devices
+  // host: 'http://ec2-34-207-196-247.compute-1.amazonaws.com:3000'
 };
