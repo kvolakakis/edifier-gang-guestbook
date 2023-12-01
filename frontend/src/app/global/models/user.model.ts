27990bfe2e45ee7ce17293656b3eb5ce9f126fb6
Konstantinos Volakakis <volakakis@ics.forth.gr>
2023-12-02 01:20:02 +0200

    Implemented forms for Login and Register (WIP: needs validation and error handling)

--- frontend/src/app/global/models/user.model.ts
+++ frontend/src/app/global/models/user.model.ts
@@ -1,12 +1,14 @@
 export class UserModel{
   _id: string;
   username: string;
+  password: string;
   createdAt: Date;
   updatedAt: Date;
 
   constructor(){
     this._id = '';
     this.username = '';
+    this.password = '';
     this.createdAt = new Date();
     this.updatedAt = new Date();
   }
--- frontend/src/app/pages/users/users.component.ts
+++ frontend/src/app/pages/users/users.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersService } from '../../global/services/users.service';
-import { firstValueFrom } from 'rxjs';
-import { UserModel } from '../../global/models/user.model';
 import { MenuComponent } from '../../shared/menu/menu.component';
+import { FormsModule } from '@angular/forms';
+import { UserModel } from '../../global/models/user.model';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, MenuComponent],
+  imports: [CommonModule, FormsModule, MenuComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
   currentUser: string | null = null;
+  username: string = '';
+  password: string = '';
+  user: UserModel = new UserModel();
+  mode: string = '';
+
   constructor(private usersService: UsersService) { }
 
   ngOnInit(){
@@ -21,9 +26,21 @@ export class UsersComponent {
   }
 
   async login(){
-    this.usersService.login({username: 'lalali'}).then((data: any) => {
-      console.log(data);
+    if(!this.username || !this.password) return;
+    this.user.username = this.username;
+    this.user.password = this.password;
+    this.usersService.login(this.user).then((data: any) => {
       this.getCurrentUser();
+    });
+  }
+
+  async register(){
+    if(!this.username || !this.password) return;
+    this.user.username = this.username;
+    this.user.password = this.password;
+    this.usersService.createUser(this.user).subscribe((data: any) => {
+      this.currentUser = data.username;
+      this.login();
     });
   }
 
@@ -36,5 +53,11 @@ export class UsersComponent {
     this.currentUser = this.usersService.getCurrentUser();
   }
 
+  setMode(mode: string){
+    this.username = '';
+    this.password = '';
+    this.user = new UserModel();
+    this.mode = mode;
+  }
 
 }
